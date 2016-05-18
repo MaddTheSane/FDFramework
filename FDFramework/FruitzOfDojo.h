70a029822c7877ee0e148432a532a853f451fe31
C.W. Betts <computers57@hotmail.com>
2016-05-18 15:37:01 -0600

    Initial framework Xcode project.

---
+++ FDFramework/FruitzOfDojo.h
@@ -0,0 +1,36 @@
+//
+//  FruitzOfDojo.h
+//  FruitzOfDojo
+//
+//  Created by C.W. Betts on 5/18/16.
+//  Copyright © 2016 C.W. Betts. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FruitzOfDojo.
+FOUNDATION_EXPORT double FruitzOfDojoVersionNumber;
+
+//! Project version string for FruitzOfDojo.
+FOUNDATION_EXPORT const unsigned char FruitzOfDojoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FruitzOfDojo/PublicHeader.h>
+
+
+#import <FruitzOfDojo/FDAudioBuffer.h>
+#import <FruitzOfDojo/FDAudioFile.h>
+#import <FruitzOfDojo/FDAudioMixer.h>
+#import <FruitzOfDojo/FDDebug.h>
+#import <FruitzOfDojo/FDDefines.h>
+#import <FruitzOfDojo/FDDisplay.h>
+#import <FruitzOfDojo/FDDisplayMode.h>
+#import <FruitzOfDojo/FDGLScreenshot.h>
+#import <FruitzOfDojo/FDHIDActuator.h>
+#import <FruitzOfDojo/FDHIDDevice.h>
+#import <FruitzOfDojo/FDHIDManager.h>
+#import <FruitzOfDojo/FDLinkView.h>
+#import <FruitzOfDojo/FDModifierCheck.h>
+#import <FruitzOfDojo/FDPreferences.h>
+#import <FruitzOfDojo/FDScreenshot.h>
+#import <FruitzOfDojo/FDView.h>
+#import <FruitzOfDojo/FDWindow.h>
