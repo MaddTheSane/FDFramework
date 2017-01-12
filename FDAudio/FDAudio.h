ab1ec6a849f3206b0db95dc798a4c4b9e0fb2323
C.W. Betts <computers57@hotmail.com>
2017-01-12 14:45:39 -0700

    Create a new framework based off of the audio code.

---
+++ FDAudio/FDAudio.h
@@ -0,0 +1,22 @@
+//
+//  FDAudio.h
+//  FDAudio
+//
+//  Created by C.W. Betts on 1/12/17.
+//  Copyright © 2017 C.W. Betts. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FDAudio.
+FOUNDATION_EXPORT double FDAudioVersionNumber;
+
+//! Project version string for FDAudio.
+FOUNDATION_EXPORT const unsigned char FDAudioVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FDAudio/PublicHeader.h>
+
+#import <FDAudio/FDDefines.h>
+#import <FDAudio/FDAudioMixer.h>
+#import <FDAudio/FDAudioBuffer.h>
+#import <FDAudio/FDAudioFile.h>
